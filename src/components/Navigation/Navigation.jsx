f978036349d9075659ea4f5beaec92a1007a6559
Maksym <mvsozykin@gmail.com>
2025-03-31 20:04:04 +0300

    Navigation, Routes, Loader, sprite, main styles added, Header done

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import { Logo } from '../Logo/Logo.jsx';
+import css from './Navigation.module.css';
+
+export const Navigation = () => {
+  const classLink = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+  return (
+    <header className={css.header}>
+      <NavLink to="/">
+        <Logo />
+      </NavLink>
+      <nav className={css.navigation}>
+        <NavLink className={classLink} to="/">
+          Home
+        </NavLink>
+        <NavLink className={classLink} to="/catalog">
+          Catalog
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
---
+++ src/pages/CarDetailsPage/CarDetailsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CarDetailsPage = () => {
+  return <div>CarDetailsPage</div>;
+};
+
+export default CarDetailsPage;
